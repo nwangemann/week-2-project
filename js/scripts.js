9721888646448a4593ceac6f36fb9fe89325d53c
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-07 10:54:14 -0700

    fix js to ensure that branches link correctly, add js logic to record quiz value and branch to results, add results pages

--- js/scripts.js
+++ js/scripts.js
@@ -7,39 +7,39 @@ $(document).ready(function() {
     $(".Question-init").hide();
     $(".Question-2").show();
   });
-  $(".takequiz").click(function() {
+  $("#btn2").click(function() {
     $(".Question-2").hide();
     $(".Question-3").show();
   });
-  $(".takequiz").click(function() {
+  $("#btn3").click(function() {
     $(".Question-3").hide();
     $(".Question-4").show();
   });
-  $(".takequiz").click(function() {
+  $("#btn4").click(function() {
     $(".Question-4").hide();
     $(".Question-5").show();
-  });
-
-  var results = $(parseInt(response1 + response2 + response3 + response4 + response5))
-
-  if (results >= 6) {
-  $(".takequiz").click(function() {
-    $(".Question-5").hide();
-    $(".ResultsFrontEnd").show();
-  } else if (results < 6 && results > 3) {
-    $(".Question-5").hide();
-    $(".ResultsApp").show();
-  } else if (results <= 3) {
-    $(".Question-5").hide();
-    $(".ResultsBackEnd").show();
-  }
   });
 });
 
 
 
-var response1 = $("input:radio[name=response1]:checked").val();
-var response2 = $("input:radio[name=response2]:checked").val();
-var response3 = $("input:radio[name=response3]:checked").val();
-var response4 = $("input:radio[name=response4]:checked").val();
-var response5 = $("input:radio[name=response5]:checked").val();
+$(document).ready(function() {
+  $("#btnfinal").click(function() {
+    $(".Question-5").hide();
+    var response1 = parseInt($("input:radio[name=response1]:checked").val());
+    var response2 = parseInt($("input:radio[name=response2]:checked").val());
+    var response3 = parseInt($("input:radio[name=response3]:checked").val());
+    var response4 = parseInt($("input:radio[name=response4]:checked").val());
+    var response5 = parseInt($("input:radio[name=response5]:checked").val());
+    var results = response1 + response2 + response3 + response4 + response5;
+
+    alert(results)
+    if (results >= 7) {
+      $(".ResultsFrontEnd").show();
+    } else if (results < 7 && results > 3) {
+      $(".ResultsApp").show();
+    } else if (results <= 3) {
+      $(".ResultsBackEnd").show();
+    }
+  });
+});

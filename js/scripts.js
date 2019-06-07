f906bc50e9d1090fc8c3a363bcbe0be1ae351978
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-07 09:57:04 -0700

    add js to progress through questions by hiding and showing next, add js to record value of input, add if else branch to evaluate result and provide a response

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
 $(document).ready(function() {
-  event.preventDefault();
   $(".takequiz").click(function() {
-    $(".Question-1").show();
+    $(".Question-init").show();
+    $(".intro").hide();
+  });
+  $("#btn1").click(function() {
+    $(".Question-init").hide();
+    $(".Question-2").show();
+  });
+  $(".takequiz").click(function() {
+    $(".Question-2").hide();
+    $(".Question-3").show();
+  });
+  $(".takequiz").click(function() {
+    $(".Question-3").hide();
+    $(".Question-4").show();
+  });
+  $(".takequiz").click(function() {
+    $(".Question-4").hide();
+    $(".Question-5").show();
+  });
+
+  var results = $(parseInt(response1 + response2 + response3 + response4 + response5))
+
+  if (results >= 6) {
+  $(".takequiz").click(function() {
+    $(".Question-5").hide();
+    $(".ResultsFrontEnd").show();
+  } else if (results < 6 && results > 3) {
+    $(".Question-5").hide();
+    $(".ResultsApp").show();
+  } else if (results <= 3) {
+    $(".Question-5").hide();
+    $(".ResultsBackEnd").show();
+  }
   });
 });
+
 
 
 var response1 = $("input:radio[name=response1]:checked").val();

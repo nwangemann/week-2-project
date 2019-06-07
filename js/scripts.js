378b23cbd585116fabd3c78ec4213398ea7b646f
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-07 08:58:49 -0700

    add first question and a replicated question template for all other questions that will be created. Define variables to collect response from radio button selection.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var response1 = $("input:radio[name=response1]:checked").val();
+var response2 = $("input:radio[name=response2]:checked").val();
+var response3 = $("input:radio[name=response3]:checked").val();
+var response4 = $("input:radio[name=response4]:checked").val();
+var response5 = $("input:radio[name=response5]:checked").val();
